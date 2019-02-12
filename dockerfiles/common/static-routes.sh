99f0e0325927237e48110da9201641a9fc397768
Kohsacu <kohsacu@email.local>
2019-02-13 04:33:11 +0900

    * refs #25: ** GoBGP Tag:v2.1.0
    	modified:   base_ubuntu18.04/Dockerfile
    	modified:   common/static-routes.sh
    	modified:   gobgp_v1/Dockerfile
    	new file:   gobgp_v2/Dockerfile
    	modified:   require/gobgpd.conf
    	new file:   require/gobgpd.sh
    
    Signed-off-by: Kohsacu <kohsacu@email.local>

--- dockerfiles/common/static-routes.sh
+++ dockerfiles/common/static-routes.sh
@@ -3,4 +3,6 @@
 #/sbin/ip route add 10.32.201.0/24  via NEXT-HOP dev eth1
 #/sbin/ip route add 172.31.11.0/24  via NEXT-HOP dev eth1
 #/sbin/ip route add 172.31.252.0/24 via NEXT-HOP dev eth1
+#/sbin/ip route add 10.255.1.0/24   via NEXT-HOP dev eth1
+#/sbin/ip route add 10.255.2.0/24   via NEXT-HOP dev eth1
 #/sbin/ip route add 10.255.3.0/24   via NEXT-HOP dev eth1
