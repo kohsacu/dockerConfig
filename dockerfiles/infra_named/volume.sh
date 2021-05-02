6eec52654ecc468b3ea2fe3f29e552552c12e3ec
Kohsacu <24363503+kohsacu@users.noreply.github.com>
2021-05-03 05:31:01 +0900

    refs #74: named (DNS Server) on docker
    * DNS サーバをコンテナで作成
    ** Base image: ubuntu:20.04
    ** bind:9.16.1-0ubuntu2.8
    
    Signed-off-by: Kohsacu <24363503+kohsacu@users.noreply.github.com>

--- dockerfiles/infra_named/volume.sh
+++ dockerfiles/infra_named/volume.sh
@@ -2,13 +2,22 @@
 
 # environment variable
 . ./.env
+TYPE=${1}
+if [ ${TYPE} = 'master' ]; then
+    SRC_DIR='1.bind-master'
+elif [ ${TYPE} = 'slave' ]; then
+    SRC_DIR='2.bind-slave'
+else
+    echo "Usage: Arg 'master' or 'slave'"
+    exit 1
+fi
 
 # bind9 drop-in files
 sudo mkdir -p "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind"
-sudo cp -ip ./files/1.bind-master/etc/named.conf                "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
-sudo cp -ip ./files/1.bind-master/etc/named.conf.internal-zones "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
-sudo cp -ip ./files/1.bind-master/etc/named.conf.local          "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
-sudo cp -ip ./files/1.bind-master/etc/named.conf.options        "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
+sudo cp -ip ./files/"${SRC_DIR}"/etc/named.conf                "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
+sudo cp -ip ./files/"${SRC_DIR}"/etc/named.conf.internal-zones "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
+sudo cp -ip ./files/"${SRC_DIR}"/etc/named.conf.local          "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
+sudo cp -ip ./files/"${SRC_DIR}"/etc/named.conf.options        "${PATH_DOCKER_VOLUME}/${CONTAINER}/etc/bind/"
 
 # bind9 Zone files
 for dir in zones-master zones-slave
